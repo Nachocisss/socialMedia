1c828256ebdc185582d7a7dd0674717bbe7d88de
Ignacio Cisternas <nachocisternas95@gmail.com>
2024-07-06 02:56:40 -0400

    left section options

--- src/components/UserIcon/UserIcon.tsx
+++ src/components/UserIcon/UserIcon.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { FaUserCircle } from "react-icons/fa";
 import "./UserIcon.css";
+
 export function UserIcon() {
   return (
     <div className="userTokenContainer">
--- src/screens/LeftMenu/LeftMenu.tsx
+++ src/screens/LeftMenu/LeftMenu.tsx
@@ -1,38 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 import "./LeftMenu.css";
 import { FaHashtag } from "react-icons/fa";
+import { FaUserCircle } from "react-icons/fa";
+import { FaHome } from "react-icons/fa";
 
 const options = [
   {
-    title: "Option 1",
-    icon: <FaHashtag size={25} fill="black" />,
+    title: "Home",
+    icon: <FaHome size={25} />,
   },
   {
-    title: "Option 2",
-    icon: <FaHashtag size={25} fill="black" />,
+    title: "My Profile",
+    icon: <FaUserCircle size={25} />,
   },
   {
     title: "Option 3",
-    icon: <FaHashtag size={25} fill="black" />,
+    icon: <FaHashtag size={25} />,
   },
 ];
 export default function LeftMenu() {
+  const [selectedIndex, setSelectedIndex] = useState(0);
   return (
     <aside className="LeftMenuContainer">
-      <div className="circle"></div>
-      {options.map((option) => {
-        return (
-          <div
-            key={`${option.title}-LeftMenu-option`}
-            className="LeftMenuOptionContainer"
-          >
-            <div className="menuItemContainer">
-              {option.icon}
-              <span className="menuItemTitle">{option.title}</span>
+      <div>
+        {options.map((option, index) => {
+          const selected = selectedIndex === index;
+          return (
+            <div
+              key={`${option.title}-LeftMenu-option`}
+              className="LeftMenuOptionContainer"
+              onClick={() => setSelectedIndex(index)}
+            >
+              <div
+                className={`menuItemContainer ${
+                  selected && "menuItemContainerSelected"
+                }`}
+              >
+                {option.icon}
+                <span className={`menuItemTitle`}>{option.title}</span>
+              </div>
+              {}
             </div>
-          </div>
-        );
-      })}
+          );
+        })}
+      </div>
     </aside>
   );
 }
